6c9f325a76b64a0e41524f6433ae003d9ffbce8b
Olya Ezhova <olya.yezhova@gmail.com>
2023-01-24 14:11:49 -0600

    Styles changes

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
-    $('.team-header-item').click(function () {
+    $('.tour-header-item').click(function () {
         $(this).addClass('active-item').siblings().removeClass('active-item');
     });
 
 
     $('.item-mountain-2').click(function () {
-        if ($('.team-slide-2').is(":hidden")) {
-            $('.team-slide-2').show();
-            $('.team-slide-1').hide();
+        if ($('.tour-slide-2').is(":hidden")) {
+            $('.tour-slide-2').show();
+            $('.tour-slide-1').hide();
+            $('.tour-slide-3').hide();
         }
     });
 
     $('.item-mountain-1').click(function () {
-        if ($('.team-slide-1').is(":hidden")) {
-            $('.team-slide-1').show();
-            $('.team-slide-2').hide();
+        if ($('.tour-slide-1').is(":hidden")) {
+            $('.tour-slide-1').show();
+            $('.tour-slide-2').hide();
+            $('.tour-slide-3').hide();
+        }
+    });
+
+    $('.item-mountain-3').click(function () {
+        if ($('.tour-slide-3').is(":hidden")) {
+            $('.tour-slide-3').show();
+            $('.tour-slide-1').hide();
+            $('.tour-slide-2').hide();
         }
     });
 });
